2b3317bfe303889d22cf6ab907f56831b8e8c5e7
YevgenYakubovskyi <yakubovskyi13@gmail.com>
2024-07-08 11:48:06 +0300

    end

--- scripts.js
+++ scripts.js
@@ -1,10 +1,26 @@
-// Плавне прокручування до секцій
-document.querySelectorAll('.nav-link').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
+function sendEmail(){
+    let params = {
+        from_name: document.getElementById('from_name').value,
+        email:document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+        from_cod: document.getElementById('from_cod').value,
+        to_cod: document.getElementById('to_cod').value,
+        message: document.getElementById('message').value,
+    };
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+    emailjs.send("service_lww2olu","template_rw9a1s7", params).then(
+        (response) => {
+            alert('SUCCESS!', response.status, response.text);
+            document.getElementById('from_name').value = '';
+            document.getElementById('email').value = '';
+            document.getElementById('phone').value = '';
+            document.getElementById('from_cod').value = '';
+            document.getElementById('to_cod').value = '';
+            document.getElementById('message').value = '';
+        },
+        (error) => {
+            alert('FAILED...', error);
+        },
+    );
+
+}
